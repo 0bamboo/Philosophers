3982fad31afca81eefbc2c0013824ce36f569a37
nowl01 <itsabdennacerama@gmail.com>
2021-09-23 16:49:35 +0100

    last

--- header/philosophers.h
+++ header/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 16:21:31 by abdait-m          #+#    #+#             */
-/*   Updated: 2021/09/22 16:48:03 by abdait-m         ###   ########.fr       */
+/*   Updated: 2021/09/22 16:57:12 by abdait-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 #define _PHILOSOPHERS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #endif
